5b2e194b753c24c36868e88f9dfe47f22fb4bf78
Hu Chengzhen <huchengzhen@gmail.com>
2023-02-12 06:44:05 +0800

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by 胡诚真 on 2023/2/12.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
